f63055b399c399309c0423ee6358da716f31f2b6
Matteo Martini <47110912+EvangMM@users.noreply.github.com>
2025-07-21 13:37:17 +0200

    fix: step_outputs

--- s3-scikit-learn/src/ml_pipeline.py
+++ s3-scikit-learn/src/ml_pipeline.py
@@ -3,5 +3,5 @@ from digitalhub_runtime_kfp.dsl import pipeline_context
 
 def myhandler():
     with pipeline_context() as pc:
-        downloader = pc.step(name="download-data", function="data-prep", action="job", outputs={"dataset": "dataset"})
+        downloader = pc.step(name="download-data", function="data-prep", action="job", step_outputs={"dataset": "dataset"})
         trainer = pc.step(name="train-model", function="train", action="job", inputs={"di": downloader.outputs["dataset"]})
