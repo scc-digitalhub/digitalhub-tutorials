315b83033affa05a579b5cbc38fc7b78aeb64277
EvangMM <matteomartini92@yahoo.it>
2026-08-07 11:11:12 +0200

    chore: small fixes

--- s6-custom-ml-model/src/pipeline.py
+++ s6-custom-ml-model/src/pipeline.py
@@ -5,25 +5,15 @@ from hera.workflows import DAG, Workflow
 def pipeline():
     with Workflow(entrypoint="dag") as w, DAG(name="dag"):
         A = step(
-            template={
-                "action": "build",
-                "instructions": ["pip3 install torch'<2.6.0' darts==0.30.0 patsy"],
-            },
-            function="train-time-series-model",
+            template={"action": "build"},
+            function="build-function-darts",
         )
         B = step(
-            template={
-                "action": "build",
-                "instructions": ["pip3 install torch'<2.6.0' darts==0.30.0 patsy"],
-            },
-            function="serve-time-series-model",
-        )
-        C = step(
             template={"action": "job"},
             function="train-time-series-model",
             outputs=["model"],
         )
-        D = step(
+        C = step(
             template={
                 "action": "serve",
                 "init_parameters": {"model_key": "{{inputs.parameters.model}}"},
@@ -31,5 +21,5 @@ def pipeline():
             function="serve-time-series-model",
             inputs={"model": C.get_parameter("model")},
         )
-        [A, B] >> C >> D
+        A >> B >> C
     return w
